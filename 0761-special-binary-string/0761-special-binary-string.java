64429db94844b7871eaae15a20dad097b562165d
ALLADI DIVYA MANASWINI <divyamanaswini2006@gmail.com>
2026-02-20 22:29:38 +0530

    Time: 2 ms (100%), Space: 43.2 MB (79.62%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String makeLargestSpecial(String s) {
+        List<String> list = new ArrayList<>();
+        int count = 0, start = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') count++;
+            else count--;
+
+            if (count == 0) {
+                String inner = s.substring(start + 1, i);
+                list.add("1" + makeLargestSpecial(inner) + "0");
+                start = i + 1;
+            }
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : list) {
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+}
