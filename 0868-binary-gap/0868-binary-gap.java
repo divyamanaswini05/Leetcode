aa3cd7a34fb48c33ca19cad42c873414e3afde29
ALLADI DIVYA MANASWINI <divyamanaswini2006@gmail.com>
2026-02-22 23:31:03 +0530

    Time: 0 ms (100%), Space: 42.6 MB (14.06%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int binaryGap(int n) {
+        int last = -1;
+        int max = 0;
+        int pos = 0;
+        
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (last != -1) {
+                    max = Math.max(max, pos - last);
+                }
+                last = pos;
+            }
+            pos++;
+            n >>= 1;
+        }
+        
+        return max;
+    }
+}
