e20a921c1e2552cfdb0e577ea8e9e7135d4c826d
ALLADI DIVYA MANASWINI <divyamanaswini2006@gmail.com>
2026-02-24 15:41:25 +0530

    Time: 1 ms (99.73%), Space: 94.5 MB (50.45%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mxp=0;
+        int mnc=prices[0];
+        for(int i=0;i<prices.length;i++){
+            mxp=Math.max(mxp,prices[i]-mnc);
+            mnc=Math.min(mnc,prices[i]);
+        }
+        return mxp;
+    }
+}
